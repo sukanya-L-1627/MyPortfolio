a8b6e0ced8cb90981ffb3ba01f26efd1ecb94992
Lambaday Sukanya <sukanyal1627@gmail.com>
2026-05-21 15:27:45 +0530

    Initial commit to MyPortfolio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -28,6 +28,8 @@ const Footer = () => {
                         <ul>
                             <li><a href="#home">Home</a></li>
                             <li><a href="#about">About</a></li>
+                            <li><a href="#education">Education</a></li>
+                            <li><a href="#achievements">Achievements</a></li>
                             <li><a href="#services">Services</a></li>
                             <li><a href="#skills">Skills</a></li>
                             <li><a href="#portfolio">Projects</a></li>
